5b86ae798a8584720f24645d2ca870a9ea072a92
AndreToral <jrodriguezt16@upao.edu.pe>
2026-09-12 19:52:55 -0500

    s

---
+++ contracts/5_VariableSimples.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.2 <0.9.0;
+
+contract VariableSimples{
+    uint256 private edad;  //por defecto, toma el valor cero
+
+    function cambiarEdad(uint256 nuevaEdad) public {
+        edad = nuevaEdad;
+    }
+
+    function devolverEdad() public view returns (uint256){
+        return edad;
+    }
+
+}
